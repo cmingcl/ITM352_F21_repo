36db6e32345130a377a4731a82ab1886664bfed8
cmingcl <cmcl@hawaii.edu>
2021-09-28 03:33:53 -1000

    lab6 commit

---
+++ Lab6/Ex1.js
@@ -0,0 +1,10 @@
+var age = 20
+var fav_num = 8
+var day_of_birth = 11
+var month_of_birth = 6
+
+console.log(age + fav_num / day_of_birth * month_of_birth);
+
+console.log( (age + fav_num) / day_of_birth * month_of_birth);
+
+// console.log((age + fav_num) / day_of_birth * month_of_birth);
