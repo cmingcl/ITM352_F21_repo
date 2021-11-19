b798ecd83ad84693c774816cdfaef4382e573ca2
cmingcl <cmcl@hawaii.edu>
2021-11-18 22:31:47 -1000

    assignment 1

---
+++ <Cheng-Leever>_<Cheung-Ming>_Assignment1/public/product_data.js
@@ -0,0 +1,49 @@
+//Creating the array of products
+//Took from SmartPhoneProducts3
+
+var products = [
+    {    
+            //Products 1 
+                "product" : "Obsidian",
+                "price" : 12.00,
+                "image": "./images/obsidian.png",
+                "quantity_available": 10 
+    },
+    {
+            //Products 2 
+                "product" : "Pyrite",
+                "price" : 15.00,
+                "image" : "./images/pyrite.png",
+                "quantity_available": 10 
+    },
+    {
+            //Products 3 
+                "product" : "Red Jasper",
+                "price" : 13.00,
+                "image" : "./images/red_jasper.png",
+                "quantity_available": 10 
+    }, 
+    {
+            //Products 4 
+                "product" : "Rose Quartz",
+                "price" : 14.00,
+                "image" : "./images/rose_quartz.png",
+                "quantity_available": 10 
+    },
+    {
+        //Products 5 
+            "product" : "Smokey Quartz",
+            "price" : 12.00,
+            "image" : "./images/smokey_quartz.png",
+            "quantity_available": 10 
+},
+    
+    ];
+    
+    //If the module is not undefined, have the module export the data from the product_data array
+    //From Assignment1_Design_Examples > Asssignment1_2file > product_data.js 
+    if(typeof module != 'undefined') { //if the type of the module is defined
+        exports.products = products; //export the product_data
+      }
+
+    
