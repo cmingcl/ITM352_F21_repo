9caf6cba15de1fd7b78458e330823b836ed1a7b4
cmingcl <cmcl@hawaii.edu>
2022-03-09 21:30:35 -1000

    lab10

---
+++ ITM352_S22/Lab10/Ex1.js
@@ -0,0 +1,16 @@
+day = 11;
+month = "June";
+year = 2001; 
+
+step1 = year - 2000;
+step2 = parseInt(step1/4);
+step3 = step2 + step1;
+step4 = 4;
+step6 = step4 + step3;
+step7 = day + step6;
+step8 = step7;
+step9 = step8 - 1;
+step10 = step9 % 7;
+
+
+console.log(step10);
